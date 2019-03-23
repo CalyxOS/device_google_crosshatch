3c9f44733e39574149469416086abcfdd8752359
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ calyx_blueline.mk
@@ -0,0 +1,10 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_blueline
+PRODUCT_MODEL := Pixel 3
+PRODUCT_RESTRICT_VENDOR_FILES := false
