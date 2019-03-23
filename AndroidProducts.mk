65c803cf4a269e8daa40ab7c6f43d27da2408c93
Chirayu Desai <chirayudesai1@gmail.com>
2019-03-23 17:29:23 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit 267d940ca189a3517a0e421ce263ad066e894014
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 02:47:29 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: I6c0ef97339817acf761cbb58855d2bd77a7aea27
        (cherry picked from commit 3dde7572de4c5f673ec254399c13137a9a570674)
    
    commit 615841a69882cd1c7c61b0987799f569e06f51b8
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:06:09 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I51f22c60c43f6a33d7c0edfb37e3f28f90924ba8
        (cherry picked from commit 170e10caf7a41c6c3911cb1b8c20709ecd8aa1d7)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit b725037b3da7e58411570a2da63444f47e75c260)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,8 +20,12 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
     $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
     $(LOCAL_DIR)/vf/aosp_crosshatch_vf.mk \
+    $(LOCAL_DIR)/calyx_crosshatch.mk \
+    $(LOCAL_DIR)/calyx_blueline.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_crosshatch-userdebug \
     aosp_blueline-userdebug \
     aosp_crosshatch_vf-userdebug \
+    calyx_crosshatch-userdebug \
+    calyx_blueline-userdebug
