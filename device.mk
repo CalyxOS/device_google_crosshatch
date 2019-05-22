1d2373dfdd075af4e4f6ec5f9732c4f17c40c83f
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-22 15:13:23 -0700

    crosshatch: Add privapp permissions for Google EUICC apps
    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

--- device.mk
+++ device.mk
@@ -867,3 +867,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/crosshatch/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
