170e10caf7a41c6c3911cb1b8c20709ecd8aa1d7
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-03 03:06:09 +0530

    Add overlay-calyx to the list of overlays
    Change-Id: I51f22c60c43f6a33d7c0edfb37e3f28f90924ba8

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-calyx
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
