b1549df21cea41adeba69886e65c1c00869b2ea3
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-09 03:04:59 +0530

    Squashed merge of staging/android11_rebase-android-11.0.0_r19
    Change-Id: Ic82b0a725efc9304921beaa74ee63458e80eada0

---
+++ calyx_crosshatch.mk
@@ -0,0 +1,11 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/crosshatch/device-calyx.mk)
+$(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_crosshatch
+PRODUCT_MODEL := Pixel 3 XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
---
+++ device-calyx.mk
@@ -0,0 +1,6 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-calyx
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/crosshatch/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
