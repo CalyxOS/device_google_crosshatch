fa46fba67421e895b6e5eab77bede5d0a5b6fdb7
Michael Bestas <mkbestas@gmail.com>
2021-01-19 18:07:04 +0200

    Conditionally revert "crosshatch: Compile kernel in-line"
    Support compiling with prebuilt kernel again.
    
    This partially reverts commit 197752f11a42932e809021f2150399d1c550230e.
    
    Change-Id: Ie9395ca3b35dd4ffda393bee90ef0d71728974d9

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -1,5 +1,6 @@
 # Inline kernel building
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+ifeq ($(INLINE_KERNEL_BUILDING),true)
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
@@ -9,3 +10,4 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
+endif
