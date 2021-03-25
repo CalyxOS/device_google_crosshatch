ef2fc8e6b843d9ed210a715da5d30c0a8b0fc336
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: I6df25bd65288925211b312e65cea7cfb59abab90

--- calyx_crosshatch.mk
+++ calyx_crosshatch.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/calyx/config/common.mk)
 $(call inherit-product, device/google/crosshatch/device-calyx.mk)
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/crosshatch/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_crosshatch
 PRODUCT_MODEL := Pixel 3 XL
