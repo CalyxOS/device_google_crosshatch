c1345813cbb4865d10370eb6344b20c9b7a7a63d
Michael Bestas <mkbestas@gmail.com>
2021-09-23 00:35:25 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 6e2117a045ca0e2ae6626290774b226386e7240d)

--- calyx_crosshatch.mk
+++ calyx_crosshatch.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_crosshatch
 PRODUCT_MODEL := Pixel 3 XL
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=crosshatch \
+    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C1 8029091 release-keys"
+
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C1/8029091:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/crosshatch/crosshatch-vendor.mk)
