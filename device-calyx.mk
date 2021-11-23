070800f7f0f99f07886a9fb1bc2e8181483a319d
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    b1c1: Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,5 +1,10 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-calyx
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # Elmyra
 PRODUCT_PACKAGES += \
     ElmyraService
