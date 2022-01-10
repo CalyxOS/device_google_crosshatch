eea142ec9c48bf47d15513648c38b7027009e20a
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-11 05:27:47 +0530

    [SQUASH] Merge tag 'android-12.0.0_r25' into staging/android12_merge-android-12.0.0_r25
    Android 12.0.0 release 25
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCYdTCUQAKCRDorT+BmrEO
    # eHtGAJ4w/4MYyLzyxGGU0YfSjxAF7yuYDgCdHWUHR5fEuwu8lm71uRb0ooPNU9M=
    # =wEIu
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Wed Jan  5 03:25:29 2022 IST
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [full]
    # gpg: initial-contribution@android.com: Verified 428 signatures in the past
    #      5 months.  Encrypted 0 messages.
    
    # By Neha Jain (1) and Paul Scovanner (1)
    # Via Android Build Coastguard Worker
    * tag 'android-12.0.0_r25':
      Update Crosshatch SVN to 60
      Update Crosshatch SVN to 59
    
    Change-Id: I0992411f40e6d2f0b516f28c5e1d9e7a7a5688d9

--- device.mk
+++ device.mk
@@ -844,7 +844,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=58
+    ro.vendor.build.svn=60
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
