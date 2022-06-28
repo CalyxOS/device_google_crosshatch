393ae68f5102ed36ebc174a5b2e9347a7f66f65b
Chirayu Desai <chirayudesai1@gmail.com>
2022-06-28 18:47:55 +0530

    Update build fingerprint and desc for Jun 22 build - SP1A.210812.016.C2
    Change-Id: I55c9201515d6156ddc6f68333e2a9853cbcb306a

--- calyx_crosshatch.mk
+++ calyx_crosshatch.mk
@@ -12,9 +12,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C1 8029091 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
 
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C1/8029091:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
