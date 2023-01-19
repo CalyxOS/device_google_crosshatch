9d425cab5fd8173835e36bb70885397bd667e563
Michael Bestas <mkbestas@gmail.com>
2023-01-19 21:34:00 +0200

    Replace EuiccGoogle with OpenEUICC
    Change-Id: I7738ff9bc2f7b98d5a91510f69bd65691cddab80

--- device-calyx.mk
+++ device-calyx.mk
@@ -5,9 +5,11 @@ PRODUCT_PACKAGES += \
     ElmyraService
 
 # EUICC
+PRODUCT_PACKAGES += \
+    OpenEUICC
+
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    device/google/crosshatch/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # For Google Camera
 PRODUCT_COPY_FILES += \
