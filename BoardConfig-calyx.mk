28712dc7560395ea0c50b4176de8505c4e8aab31
Michael Bestas <mkbestas@gmail.com>
2023-07-29 13:22:01 +0300

    Revert "crosshatch: Build Lineage Health HAL"
    msm-4.9 doesn't have the google_battery kernel driver.
    
    This reverts commit bd52cdaeb0dab5071dfa75a3b4e1f1d80158f20e.
    
    Change-Id: I60db6311464bcd61e1e66dcad51de3c7b7d6fd13

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -6,10 +6,6 @@
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
-
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/calyx/config/device_framework_matrix.xml
